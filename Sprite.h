fecf8d499f0432561884efa5e1e0ef4c984e736d
Axel874 <axel.avez@student.howest.be>
2021-03-15 10:11:15 +0100

    spritemanager initial setup

--- Sprite.h
+++ Sprite.h
@@ -1,17 +1,13 @@
 #pragma once
 class Sprite {
 public:
-	Sprite(const std::string& src, const glm::vec3& position, const glm::vec2& dimensions);
-	Sprite(const std::string& src, float x, float y, float z, float w, float h);
-
-	float* GetVertexData();
+	Sprite(const std::string& src, const glm::vec3& position, const glm::vec2& scale);
+	Sprite(const std::string& src, float x, float y, float z, float sx, float sy);
 	std::string GetSource() const;
-protected:
-	glm::vec3 m_Position;
+	glm::vec2 GetSize() const;
+	glm::vec3 getPosition() const;
 private:
-	float m_VertexData[24];
+	glm::vec3 m_Position;
+	glm::vec2 m_Size;
 	std::string m_Src;
-	glm::vec2 m_Dimensions;
-
-	void UpdateVertexData();
 };
